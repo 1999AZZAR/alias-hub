7f41bd6801486ec312e590afc7c280b7cfc78a83
1999AZZAR <1999azzar@gmail.com>
2025-05-05 13:33:03 +0700

    ..

--- helpers.sh
+++ helpers.sh
@@ -3,20 +3,24 @@
 # Remove alias eval to avoid overriding built-in eval
 unalias eval 2>/dev/null
 
-# apt wrapper (with sudo)
-apt() { sudo apt "$@"; }
-
-# snap wrapper (with sudo)
-snap() { sudo snap "$@"; }
-
 # docker wrapper
 docker_cmd() { docker "$@"; }
 
 # Docker shortcut alias
 alias d='docker'
 
-# flatpak wrapper
-flatpak() { flatpak "$@"; }
+apt() { sudo /usr/bin/apt "$@"; }
+snap() { sudo /usr/bin/snap "$@"; }
+flatpak() {
+  case "$1" in
+  install | update | remove | override | repair | uninstall)
+    sudo /usr/bin/flatpak "$@"
+    ;;
+  *)
+    /usr/bin/flatpak "$@"
+    ;;
+  esac
+}
 
 # Kubernetes shortcut alias
 alias k='kubectl'
@@ -34,7 +38,15 @@ update() { apt update && apt upgrade -y && apt autoremove -y && snap refresh && 
 dis_update() { apt update && apt upgrade -y && apt dist-upgrade -y && sudo do-release-upgrade; }
 
 # Plymouth boot splash
-boot_splash() { sudo plymouthd; sudo plymouth --show-splash; for ((i=0; i<5; i++)); do sleep 1; sudo plymouth --update=test$i; done; sudo plymouth --quit; }
+boot_splash() {
+  sudo plymouthd
+  sudo plymouth --show-splash
+  for ((i = 0; i < 5; i++)); do
+    sleep 1
+    sudo plymouth --update=test$i
+  done
+  sudo plymouth --quit
+}
 
 # ASCII art wrapper: use looper.sh for options, else figlet if available
 ascii() {
@@ -71,7 +83,7 @@ extract() {
   for archive in "$@"; do
     if [ -f "$archive" ]; then
       case $archive in
-        *.tar.bz2) tar xvjf "$archive" ;;  *.tar.gz) tar xvzf "$archive" ;;  *.bz2) bunzip2 "$archive" ;;  *.rar) unrar e -r "$archive" ;;  *.gz) gunzip "$archive" ;;  *.tar) tar xvf "$archive" ;;  *.tbz2) tar xvjf "$archive" ;;  *.tgz) tar xvzf "$archive" ;;  *.zip) unzip -q "$archive" ;;  *.Z) uncompress "$archive" ;;  *.7z) 7z x "$archive" ;;  *) echo "Don't know how to extract '$archive'..." ;;
+      *.tar.bz2) tar xvjf "$archive" ;; *.tar.gz) tar xvzf "$archive" ;; *.bz2) bunzip2 "$archive" ;; *.rar) unrar e -r "$archive" ;; *.gz) gunzip "$archive" ;; *.tar) tar xvf "$archive" ;; *.tbz2) tar xvjf "$archive" ;; *.tgz) tar xvzf "$archive" ;; *.zip) unzip -q "$archive" ;; *.Z) uncompress "$archive" ;; *.7z) 7z x "$archive" ;; *) echo "Don't know how to extract '$archive'..." ;;
       esac
       echo "Extracted: $archive"
     else
@@ -82,10 +94,14 @@ extract() {
 
 # Create archives
 archive() {
-  if [ -z "$1" ] || [ -z "$2" ]; then echo "Usage: archive [archive_name.ext] [file/dir1] [file/dir2] ..."; return 1; fi
-  local archive_name="$1"; shift
+  if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage: archive [archive_name.ext] [file/dir1] [file/dir2] ..."
+    return 1
+  fi
+  local archive_name="$1"
+  shift
   case "$archive_name" in
-    *.tar.gz|*.tgz) tar -czvf "$archive_name" "$@" ;;  *.tar.bz2|*.tbz) tar -cjvf "$archive_name" "$@" ;;  *.tar) tar -cvf "$archive_name" "$@" ;;  *.zip) zip -r "$archive_name" "$@" ;;  *.7z) 7z a "$archive_name" "$@" ;;  *.rar) rar a "$archive_name" "$@" ;;  *) echo "Unsupported archive format: $archive_name" ;;
+  *.tar.gz | *.tgz) tar -czvf "$archive_name" "$@" ;; *.tar.bz2 | *.tbz) tar -cjvf "$archive_name" "$@" ;; *.tar) tar -cvf "$archive_name" "$@" ;; *.zip) zip -r "$archive_name" "$@" ;; *.7z) 7z a "$archive_name" "$@" ;; *.rar) rar a "$archive_name" "$@" ;; *) echo "Unsupported archive format: $archive_name" ;;
   esac
   [ $? -eq 0 ] && echo "Created archive: $archive_name"
 }
@@ -104,7 +120,9 @@ back() {
   if [ -z "$1" ]; then
     builtin cd - && ls
   else
-    local dirs="cd "; for ((i=0; i<$1; i++)); do dirs+="../"; done; eval "$dirs" && ls
+    local dirs="cd "
+    for ((i = 0; i < $1; i++)); do dirs+="../"; done
+    eval "$dirs" && ls
   fi
 }
 
@@ -116,22 +134,12 @@ findc() { find . -type f -exec grep -l "$1" {} \;; }
 
 # batch rename files
 rename_ext() {
-  if [ $# -ne 2 ]; then echo "Usage: rename-ext old_extension new_extension"; return 1; fi
+  if [ $# -ne 2 ]; then
+    echo "Usage: rename-ext old_extension new_extension"
+    return 1
+  fi
   find . -name "*.$1" -exec bash -c 'mv "$1" "${1%.$2}.$3"' - {} "$1" "$2" \;
 }
-
-# Auto-completion for alias-list command
-_alias_list_completions() {
-  local current_word="${COMP_WORDS[COMP_CWORD]}"
-  local matches
-  if [ -d "${ALIASES_DIR}" ]; then
-    matches=$(compgen -W "$(find "${ALIASES_DIR}" -name "*.alias" -type f -printf "%f\n" | sed 's/\.alias$//')" -- "${current_word}")
-    COMPREPLY=(${matches})
-  fi
-}
-
-# Enable completion for alias-list
-complete -F _alias_list_completions alias-list
 
 # Curl wrapper
 cw() { curl -s "$@"; }
@@ -148,11 +156,11 @@ trending_coins() { cw https://api.coingecko.com/api/v3/search/trending | jq .; }
 # Random fun generator
 fun() {
   case "$1" in
-    dice) echo $((RANDOM % 6 + 1)) ;;
-    coin) echo $((RANDOM % 2 == 0 ? "Heads" : "Tails")) ;;
-    color) echo "[38;5;$((RANDOM%256))m█[0m" ;;
-    quote) cw http://quotes.stormconsultancy.co.uk/random.json | jq -r .quote ;;
-    joke) cw https://icanhazdadjoke.com/ | jq -r .joke ;;
-    *) echo "Usage: fun {dice|coin|color|quote|joke}" ;;
+  dice) echo $((RANDOM % 6 + 1)) ;;
+  coin) echo $((RANDOM % 2 == 0 ? "Heads" : "Tails")) ;;
+  color) echo "[38;5;$((RANDOM % 256))m█[0m" ;;
+  quote) cw http://quotes.stormconsultancy.co.uk/random.json | jq -r .quote ;;
+  joke) cw https://icanhazdadjoke.com/ | jq -r .joke ;;
+  *) echo "Usage: fun {dice|coin|color|quote|joke}" ;;
   esac
 }
